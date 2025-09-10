e1611facd7da71ec35b6dbaafc7c1784c443f928
Chatethakhun Khongthon <chatethakhunkhongthon@Chatethakhuns-MacBook-Air.local>
2025-09-10 15:49:41 +0700

    add api get single workout session

--- controllers/workoutSessionController.js
+++ controllers/workoutSessionController.js
@@ -79,3 +79,20 @@ export const deleteWorkoutSession = async (req, res) => {
     res.status(500).send(error);
   }
 };
+
+export const getSingleWorkoutSession = async (req, res) => {
+  try {
+    const workoutSession = await WorkoutSession.findById(req.params.id);
+
+    if (!workoutSession) {
+      res.status(404).send("Workout session not found");
+      return;
+    }
+
+    res.status(200).json({
+      workoutSession,
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
--- routes/workoutSessionRoutes.js
+++ routes/workoutSessionRoutes.js
@@ -4,12 +4,14 @@ import {
   endWorkoutSession,
   getListWorkoutSession,
   deleteWorkoutSession,
+  getSingleWorkoutSession,
 } from "../controllers/workoutSessionController.js";
 import { protectedRoute } from "../middleware/auth.js";
 
 const workoutRouter = express.Router();
 
 workoutRouter.get("/list", protectedRoute, getListWorkoutSession);
+workoutRouter.get("/:id", protectedRoute, getSingleWorkoutSession);
 workoutRouter.post("/start", protectedRoute, startWorkoutSession);
 workoutRouter.put("/end/:id", protectedRoute, endWorkoutSession);
 workoutRouter.delete("/:id", protectedRoute, deleteWorkoutSession);
