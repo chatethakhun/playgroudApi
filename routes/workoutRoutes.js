b2136d8e7b8169b4ccaaa31048a2b4016f907410
Chatethakhun Khongthon <chatethakhunkhongthon@Chatethakhuns-MacBook-Air.local>
2025-09-11 12:22:44 +0700

    add protected route

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import * as workoutController from "../controllers/workoutController.js";
+import { protectedRoute } from "../middleware/auth.js";
 const workOutRouter = express.Router();
 
-workOutRouter.get("/list", workoutController.getListWorkout);
-workOutRouter.post("/create", workoutController.createWorkout);
-workOutRouter.get("/:id", workoutController.getSingleWorkout);
-workOutRouter.put("/:id", workoutController.updateWorkout);
-workOutRouter.delete("/:id", workoutController.deleteWorkout);
+workOutRouter.get("/list", protectedRoute, workoutController.getListWorkout);
+workOutRouter.post("/create", protectedRoute, workoutController.createWorkout);
+workOutRouter.get("/:id", protectedRoute, workoutController.getSingleWorkout);
+workOutRouter.put("/:id", protectedRoute, workoutController.updateWorkout);
+workOutRouter.delete("/:id", protectedRoute, workoutController.deleteWorkout);
 
 export default workOutRouter;
