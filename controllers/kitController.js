c49a83f2bfc1b5babfb4c9940e0b84b5b50b0835
“The <chatethakhun.jack@gmail.com>
2025-09-14 13:58:51 +0700

    Add GET endpoint for single kit retrieval

--- controllers/kitController.js
+++ controllers/kitController.js
@@ -13,3 +13,11 @@ export const getKits = async (req, res) => {
   const kits = await Kit.find().forUser(req.user.id).lean();
   res.json(kits);
 };
+
+export const getKit = async (req, res) => {
+  const kit = await Kit.findOne({ _id: req.params.id })
+    .forUser(req.user.id)
+    .lean();
+  if (!kit) return res.status(404).json({ error: "Not found" });
+  res.json(kit);
+};
--- routes/kitRoutes.js
+++ routes/kitRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createKit, getKits } from "../controllers/kitController.js";
+import { createKit, getKit, getKits } from "../controllers/kitController.js";
 import { protectedRoute } from "../middleware/auth.js";
 
 const kitRouter = Router();
@@ -7,5 +7,7 @@ const kitRouter = Router();
 kitRouter.post("/", protectedRoute, createKit);
 
 kitRouter.get("/", protectedRoute, getKits);
+
+kitRouter.get("/:id", protectedRoute, getKit);
 
 export default kitRouter;
