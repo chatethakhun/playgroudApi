bd97d69d317e835910fe5213a012c9013b9a22bc
“The <chatethakhun.jack@gmail.com>
2025-09-26 22:05:57 +0700

    Populate requires.runner in updateCutInRequires

--- controllers/partController.js
+++ controllers/partController.js
@@ -124,7 +124,9 @@ export const deletePart = async (req, res) => {
 export const updateCutInRequires = async (req, res) => {
   const { id, runnerId } = req.params;
 
-  const part = await Part.findOne({ _id: id }).forUser(req.user.id);
+  const part = await Part.findOne({ _id: id })
+    .forUser(req.user.id)
+    .populate("requires.runner");
   if (!part) return res.status(404).json({ error: "Part not found" });
 
   // toggle หรือ set true ก็ได้
